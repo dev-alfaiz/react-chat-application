10a5931d6b8671ee59ae43ea29fa5c928e5713aa
dev-alfaiz <captainalfaiz@gmail.com>
2022-06-27 16:43:09 +0530

    configure backend

--- src/App.js
+++ src/App.js
@@ -1,7 +1,12 @@
 import * as React from "react";
+import "./App.css";
 
 const App = () => {
-  return <div className="app">React Chat Application</div>;
+  return (
+    <div className="App">
+      <h1>React Chat Application</h1>
+    </div>
+  );
 };
 
 export default App;
